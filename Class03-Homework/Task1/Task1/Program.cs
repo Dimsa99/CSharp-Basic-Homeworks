3e0db0d7b394a51bf0f379c85a3f520d6fe7c477
Dimsa99 <dimsajosifovski@gmail.com>
2022-03-17 16:08:53 +0100

    Homework number two!

---
+++ Class03-Homework/Task1/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Make a console application called SumOfEven. " +
+                "Inside it create an array of 6 integers. Get numbers from the input, " +
+                "find and print the sum of the even numbers from the array:");
+
+
+            Console.WriteLine("SumOfEven");
+            int sum = 0;
+            int[] array = new int[6];
+            Console.WriteLine("Enter 6 numbers!");
+            for (int i = 0; i < array.Length; i++)
+            {
+                //array[i] = Console.Read();
+                //Console.WriteLine(array[i]);
+                //if(array[i] %2 == 0)
+                //{
+
+                //}
+
+                
+                string input = Console.ReadLine();
+                int.TryParse(input, out int parsedInput);
+                array[i] = parsedInput;
+                if(parsedInput % 2 == 0)
+                {
+                    sum += parsedInput;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine(sum);
+            
+
+        }
+    }
+}
---
+++ Class03-Homework/Task2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student group!");
+            Console.WriteLine("Please enter 1 or 2 for which group you want to print out.");
+            string input;
+            input = Console.ReadLine();
+            string[] groupOne = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] groupTwo = { "Zoki", "Mile", "Marko", "Martin", "Vojdan" };
+            
+            switch (input)
+            {
+                case "1":
+                    {
+                        foreach (string person in groupOne)
+                        {
+                            Console.WriteLine(person);
+                        }
+                    }
+                    break;
+                case "2":
+                    {
+                        foreach (string person in groupTwo)
+                        {
+                            Console.WriteLine(person);
+                        }
+                        break;
+
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                    break;
+            }
+        }
+    }
+}
